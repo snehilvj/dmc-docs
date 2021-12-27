eb7b882e2b177eb0e5ae800db1de9a828ea7a4cd
Snehil Vijay <snehilvj@outlook.com>
2021-12-27 19:56:20 +0530

    Setup demo page

--- components/accordion.py
+++ components/accordion.py
@@ -1,27 +1,21 @@
-from dash import html
 import dash_mantine_components as dmc
-from utils import create_component_title
 
+title = "Accordion"
+doc = dmc.Accordion.__doc__
 
-accordion = html.Div(
-    [
-        create_component_title("Accordion"),
-        dmc.Accordion(
-            children=[
-                dmc.AccordionItem(
-                    "Colors, fonts, shadows and many other parts are customizable to fit your design needs",
-                    label="Customization",
-                ),
-                dmc.AccordionItem(
-                    "Configure components appearance and behavior with vast amount of settings or overwrite any part of component styles",
-                    label="Flexibility",
-                ),
-                dmc.AccordionItem(
-                    "With new :focus-visible pseudo-class focus ring appears only when user navigates with keyboard",
-                    label="No annoying focus ring",
-                ),
-            ],
+layout = dmc.Accordion(
+    children=[
+        dmc.AccordionItem(
+            "Colors, fonts, shadows and many other parts are customizable to fit your design needs",
+            label="Customization",
         ),
-        dmc.Space(h=30),
-    ]
+        dmc.AccordionItem(
+            "Configure components appearance and behavior with vast amount of settings or overwrite any part of component styles",
+            label="Flexibility",
+        ),
+        dmc.AccordionItem(
+            "With new :focus-visible pseudo-class focus ring appears only when user navigates with keyboard",
+            label="No annoying focus ring",
+        ),
+    ],
 )
--- utils.py
+++
@@ -1,5 +0,0 @@
-import dash_mantine_components as dmc
-
-
-def create_component_title(label):
-    return dmc.Text(label, style={"margin": "15px 0"}, size="xl")
