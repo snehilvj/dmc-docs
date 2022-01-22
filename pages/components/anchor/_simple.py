b365b4be569b3b279666b6b75be9d195e30b10c9
Snehil Vijay <snehilvj@outlook.com>
2022-01-22 12:47:46 +0530

    Added badge, anchor

---
+++ pages/components/anchor/_simple.py
@@ -0,0 +1,6 @@
+import dash_mantine_components as dmc
+
+component = dmc.Anchor(
+    "Dash Mantine Components Announcement",
+    href="https://community.plotly.com/t/dash-mantine-components/58414",
+)
