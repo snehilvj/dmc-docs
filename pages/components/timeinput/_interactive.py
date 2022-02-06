c7bf35d5a1b369ddb8392e8bd7787ba04e1f7c07
Snehil Vijay <snehilvj@outlook.com>
2022-02-06 18:29:05 +0530

    Updated docs for timeinput

--- pages/components/timeinput/_interactive.py
+++ pages/components/timeinput/_interactive.py
@@ -12,17 +12,16 @@ component = html.Div(
                     style={"flex": 1},
                     children=[
                         dmc.TimeInput(
-                            "Input Props",
                             id="timeinput-demo",
-                            label="My label",
-                            description="My description",
-                            error="error",
+                            label="Start Time",
+                            description="This is a description.",
                             variant="default",
                             radius="sm",
                             size="sm",
                             withSeconds=False,
                             disabled=False,
                             required=True,
+                            style={"width": 150}
                         ),
                     ],
                 ),
@@ -31,24 +30,9 @@ component = html.Div(
                     direction="column",
                     grow=True,
                     children=[
-                        dmc.TextInput(
-                            id="label-timeinput-demo",
-                            value="My label",
-                            label="Label",
-                        ),
-                        dmc.TextInput(
-                            id="description-timeinput-demo",
-                            value="My description",
-                            label="Description",
-                        ),
-                        dmc.TextInput(
-                            id="error-timeinput-demo",
-                            value=None,
-                            label="Error",
-                        ),
                         DemoSelect(
                             id="variant-timeinput-demo",
-                            value="filled",
+                            value="default",
                             label="Variant",
                             data=["default", "filled", "unstyled", "headless"],
                         ),
@@ -59,8 +43,8 @@ component = html.Div(
                             id="size-timeinput-demo", label="Size", initial_value=2
                         ),
                         dmc.Switch(
-                            label="With seconds",
-                            id="withseconds-timeinput-demo",
+                            label="With Seconds",
+                            id="seconds-timeinput-demo",
                             checked=False,
                         ),
                         dmc.Switch(
--- pages/components/timeinput/timeinput.md
+++ pages/components/timeinput/timeinput.md
@@ -2,26 +2,17 @@ TimeInput | Capture time input from user.
 
 ### Simple Example
 
-This is a simple example of the TimeInput. You can enter a valid time or
-use the date object from datetime library.
+This is a simple example of the TimeInput. You can enter a valid time or use the date object from datetime library.
 
 Use the `format` prop to specify 12 or 24 hour display and the `withSeconds` prop to display seconds.
 
 > example:components/timeinput/_simple.py
 
-
 ### Input Props
 
 TimeInput supports all props from Input and InputWrapper components:
 
-> example:components/timeinput/_interactive.py:no-prism 
-
-### With icon
-
-You can display any icon on the left and control with spacing between the icon and the time display with `iconWidth`.
-
-> example:components/timeinput/_icon.py
-
+> example:components/timeinput/_interactive.py:no-prism
 
 ### Invalid state and error
 
