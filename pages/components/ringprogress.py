2a9a99e707257ea94b9eca3163e222da73b0c6af
Snehil Vijay <snehilvj@outlook.com>
2022-02-21 22:47:06 +0530

    Update to latest dmc

--- pages/components/ringprogress.py
+++ pages/components/ringprogress.py
@@ -2,6 +2,6 @@ import dash
 
 from lib.converter import convert_md_to_dash_layout
 
-dash.register_page(__name__, name="Ring Progress")
+dash.register_page(__name__, name="RingProgress")
 
 layout = convert_md_to_dash_layout("components/ringprogress/ringprogress.md")
