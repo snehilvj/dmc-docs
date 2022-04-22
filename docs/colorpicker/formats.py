0082bc2aeff9196847be1938999d5dd81459a1c1
Snehil Vijay <snehilvj@outlook.com>
2022-04-22 09:49:41 +0530

    Add docs for ColorPicker

---
+++ docs/colorpicker/formats.py
@@ -0,0 +1,35 @@
+import dash_mantine_components as dmc
+from dash import html, Input, Output, callback
+
+component = html.Div(
+    [
+        dmc.Group(
+            position="apart",
+            children=[
+                dmc.ColorPicker(id="colorpicker-format", format="hex", value="#343353"),
+                dmc.Select(
+                    id="format-select",
+                    data=[
+                        {"label": fmt.upper(), "value": fmt}
+                        for fmt in ["hex", "rgb", "rgba", "hsl", "hsla"]
+                    ],
+                    value="hex",
+                ),
+            ],
+        ),
+        dmc.Space(h=10),
+        dmc.Text(id="selected-color-format"),
+    ]
+)
+
+
+@callback(Output("colorpicker-format", "format"), Input("format-select", "value"))
+def pick_format(value):
+    return value
+
+
+@callback(
+    Output("selected-color-format", "children"), Input("colorpicker-format", "value")
+)
+def pick_color(color):
+    return color
