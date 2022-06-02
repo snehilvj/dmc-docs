e16354a507551bf7faed4ba74fd7afc08e692b30
tringuy1993 <tringuy1993@gmail.com>
2022-06-01 17:17:39 -0700

    Pagination Doc

---
+++ docs/pagination/siblings.py
@@ -0,0 +1,35 @@
+import dash_mantine_components as dmc
+
+component = dmc.Group([
+
+        dmc.Pagination(
+            total=20,
+            # direction="column",
+            # withControls=False,
+            # withEdges=False,
+            grow=True,
+            align="stretch",
+            siblings=1
+        ),
+
+        dmc.Pagination(
+            total=20,
+            # direction="column",
+            # withControls=False,
+            # withEdges=False,
+            grow=True,
+            align="stretch",
+            siblings=2
+        ),
+
+        dmc.Pagination(
+            total=20,
+            # direction="column",
+            # withControls=False,
+            # withEdges=False,
+            grow=True,
+            align="stretch",
+            siblings=3
+        )
+    ]
+)
