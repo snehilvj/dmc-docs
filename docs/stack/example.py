966e98620158e1ad146877b0505d491f119722a8
Arbol <102921107+tringuy1993@users.noreply.github.com>
2022-06-11 04:41:05 -0700

    Stack Doc (#20)

---
+++ docs/stack/example.py
@@ -0,0 +1,16 @@
+import dash_mantine_components as dmc
+
+component = dmc.Stack(
+    [
+        dmc.Button(
+            "1", variant='outline'
+        ),
+        dmc.Button(
+            "2", variant='outline'
+        ),
+        dmc.Button(
+            "3", variant='outline'
+        )
+    ],
+    align="center"
+)
