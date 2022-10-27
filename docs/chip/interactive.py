928d4a46a698ade7a87e38fa6df281c5110accbd
Snehil Vijay <snehilvj@outlook.com>
2022-10-27 14:23:03 +0530

    :hammer: Update Chip and ChipGroup docs

--- docs/chip/interactive.py
+++ docs/chip/interactive.py
@@ -6,19 +6,14 @@ controls = [
     {"property": "color", "component": "ColorPicker", "value": "#34c6ef5"},
     {
         "property": "variant",
-        "component": "SegmentedControl",
+        "component": "DemoSegmentedControl",
         "data": ["outline", "filled"],
         "value": "outline",
     },
-    {"property": "spacing", "component": "DemoSlider", "value": "xs"},
     {"property": "size", "component": "DemoSlider", "value": "sm"},
     {"property": "radius", "component": "DemoSlider", "value": "xl"},
 ]
 
-demo = dmc.Chips(
-    value="React",
-    data=[{"label": x, "value": x} for x in ["React", "Django", "Dash", "Vue"]],
-    variant="outline",
-)
+demo = dmc.Chip("Dash Mantine Components", checked=True, variant="outline")
 
 component = create_configurator(demo, controls)
--- docs/chips/multiple.py
+++
@@ -1,7 +0,0 @@
-import dash_mantine_components as dmc
-
-component = dmc.Chips(
-    data=[{"label": x, "value": x} for x in ["React", "Django", "Dash", "Vue"]],
-    value=["React", "Dash"],
-    multiple=True,
-)
