d50e4de119d2f615e109ee063c586824b4c92aba
Snehil Vijay <snehilvj@outlook.com>
2022-10-27 14:45:37 +0530

    :zap: Minor updates to Drawer docs

--- docs/drawer/placement.py
+++ docs/drawer/placement.py
@@ -1,6 +1,13 @@
 import dash_mantine_components as dmc
 from dash import html, Output, Input, State, callback
 
+data = [
+    ["Left (Default)", "left"],
+    ["Top", "top"],
+    ["Right", "right"],
+    ["Bottom", "bottom"],
+]
+
 component = html.Div(
     [
         dmc.Drawer(id="drawer-position"),
@@ -9,14 +16,9 @@ component = html.Div(
             spacing="xl",
             children=[
                 dmc.RadioGroup(
+                    [dmc.Radio(label, value=value) for label, value in data],
                     id="drawer-position-radio",
                     value="left",
-                    data=[
-                        {"label": "Left (Default)", "value": "left"},
-                        {"label": "Top", "value": "top"},
-                        {"label": "Right", "value": "right"},
-                        {"label": "Bottom", "value": "bottom"},
-                    ],
                 ),
                 dmc.Button("Open Drawer", id="drawer-position-button"),
             ],
