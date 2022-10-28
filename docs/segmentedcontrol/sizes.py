cee5e667dd5c2abb9b1bac710ddd8d4a48cc2e4b
Snehil Vijay <snehilvj@outlook.com>
2022-10-29 01:09:03 +0530

    :zap: Update docs for SegmentedControl

--- docs/segmentedcontrol/sizes.py
+++ docs/segmentedcontrol/sizes.py
@@ -2,9 +2,7 @@ import dash_mantine_components as dmc
 
 data = ["Dash", "Mantine", "Bootstrap", "Core"]
 
-component = dmc.Group(
-    direction="column",
-    children=[
-        dmc.SegmentedControl(data=data, size=x) for x in ["xs", "sm", "md", "lg", "xl"]
-    ],
+component = dmc.Stack(
+    [dmc.SegmentedControl(data=data, size=x) for x in ["xs", "sm", "md", "lg", "xl"]],
+    align="flex-start",
 )
