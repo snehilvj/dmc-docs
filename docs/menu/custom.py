f765244906c2b2b1c57fdbb92af0399be187f56a
Snehil Vijay <snehilvj@outlook.com>
2022-12-06 00:38:02 +0530

    :sparkles: Add docs for menu

---
+++ docs/menu/custom.py
@@ -0,0 +1,19 @@
+from dash_iconify import DashIconify
+import dash_mantine_components as dmc
+
+component = dmc.Menu(
+    [
+        dmc.MenuTarget(dmc.ActionIcon(DashIconify(icon="tabler:user"))),
+        dmc.MenuDropdown(
+            [
+                dmc.MenuItem(
+                    "External Link",
+                    href="https://www.github.com/snehilvj",
+                    target="_blank",
+                    icon=DashIconify(icon="radix-icons:external-link"),
+                ),
+                dmc.MenuItem("Useless Button", n_clicks=0),
+            ]
+        ),
+    ]
+)
