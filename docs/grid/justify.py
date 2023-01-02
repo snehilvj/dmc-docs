752cc7ec83655ed957f6a2e689ef4dd64b6e155a
Snehil Vijay <snehilvj@outlook.com>
2023-01-02 22:29:29 +0530

    :bug: #28 Fix grid align demo and :arrow_up: Upgrade dmc version

--- docs/grid/justify.py
+++ docs/grid/justify.py
@@ -1,11 +1,11 @@
 import dash_mantine_components as dmc
-from dash import html
 
 from lib.configurator import create_configurator
 
 style = {
     "border": f"1px solid {dmc.theme.DEFAULT_COLORS['indigo'][4]}",
     "textAlign": "center",
+    "margin": 2
 }
 
 controls = [
@@ -25,11 +25,10 @@ controls = [
 
 demo = dmc.Grid(
     children=[
-        dmc.Col(html.Div("1", style=style), span=3),
-        dmc.Col(html.Div("2", style={**style, "minHeight": 80}), span=3),
-        dmc.Col(html.Div("3", style={**style, "minHeight": 120}), span=3),
+        dmc.Col("1", style=style, span=3),
+        dmc.Col("2", style={**style, "minHeight": 80}, span=3),
+        dmc.Col("3", style={**style, "minHeight": 120}, span=3),
     ],
-    gutter="md",
 )
 
 component = create_configurator(demo, controls, center=False)
