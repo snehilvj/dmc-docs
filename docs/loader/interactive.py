133f7c487cfa0d9e8f0929c751b8177a61dd741d
Snehil Vijay <snehilvj@outlook.com>
2023-09-18 21:04:18 +0530

    :sparkles: Add docs for Loader and Kbd

---
+++ docs/loader/interactive.py
@@ -0,0 +1,15 @@
+import dash_mantine_components as dmc
+
+from components.configurator import Configurator
+
+TARGET_ID = "interactive-loader"
+
+target = dmc.Center(dmc.Loader(id=TARGET_ID))
+
+configurator = Configurator(target, TARGET_ID)
+
+configurator.add_colorpicker("color", "red")
+configurator.add_slider("size", "md")
+configurator.add_segmented_control("variant", ["oval", "bars", "dots"], "oval")
+
+component = configurator.panel
