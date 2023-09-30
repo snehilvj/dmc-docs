0ff7d57a69946c3a49e935975d0c2b2467d56019
AnnMarueW <amward@fastmail.com>
2023-09-30 08:46:02 -0700

    Add docs for ringProgress

---
+++ docs/ringprogress/interactive.py
@@ -0,0 +1,24 @@
+import dash_mantine_components as dmc
+
+from components.configurator import Configurator
+
+TARGET_ID = "interactive-ringprogress"
+
+target = dmc.RingProgress(
+    size=120,
+    thickness=12,
+    sections=[
+        {"value": 40, "color": "red"},
+        {"value": 15, "color": "yellow"},
+        {"value": 15, "color": "violet"},
+    ],
+    id=TARGET_ID
+)
+
+configurator = Configurator(target, TARGET_ID)
+
+configurator.add_number_input("size", 120, **{"min": 60, "max": 400, "step": 10})
+configurator.add_number_input("thickness", 12, **{"min": 0})
+configurator.add_switch("roundCaps", False)
+
+component = configurator.panel
