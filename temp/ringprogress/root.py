cd8eafddae91b11ee0cfe7792cd71923d9675173
AnnMarueW <amward@fastmail.com>
2023-09-30 08:47:15 -0700

    Add docs for ringProgress

--- temp/ringprogress/root.py
+++
@@ -1,8 +0,0 @@
-import dash_mantine_components as dmc
-
-component = dmc.RingProgress(
-    sections=[
-        {"value": 40, "color": "yellow"},
-    ],
-    rootColor="red",
-)
--- temp/ringprogress/tooltip.py
+++
@@ -1,9 +0,0 @@
-import dash_mantine_components as dmc
-
-component = dmc.RingProgress(
-    sections=[
-        {"value": 14, "color": "yellow", "label": "Docs", "tooltip": "Docs - 14GB"},
-        {"value": 17, "color": "red", "label": "Apps", "tooltip": "Apps - 17GB"},
-        {"value": 69, "color": "violet", "label": "Other", "tooltip": "Other - 69GB"},
-    ],
-)
