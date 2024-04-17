2318bf31e28acd0c965c5c862ef4e5ecf3830d55
Snehil Vijay <snehilvj@outlook.com>
2024-04-18 00:24:10 +0530

    Remove traces of old docs

--- docs/chip/group.py
+++
@@ -1,6 +0,0 @@
-import dash_mantine_components as dmc
-
-component = dmc.ChipGroup(
-    [dmc.Chip(x, value=x) for x in ["React", "Django", "Dash", "Vue"]],
-    value="React",
-)
--- docs/footer/simple.py
+++
@@ -1,8 +0,0 @@
-import dash_mantine_components as dmc
-
-component = dmc.Footer(
-    height=60,
-    fixed=True,
-    children=[dmc.Text("Company Logo")],
-    style={"backgroundColor": "#9c86e2"},
-)
