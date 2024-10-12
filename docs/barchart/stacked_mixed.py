6976cf1ae518bfd4ffcb3dfb59bf5c63d0b27885
AnnMarueW <amward@fastmail.com>
2024-10-12 07:06:24 -0700

    added mixed stack and waterfall examples to the BarChart section

---
+++ docs/barchart/stacked_mixed.py
@@ -0,0 +1,13 @@
+import dash_mantine_components as dmc
+from .data import data
+
+component = dmc.BarChart(
+    h=300,
+    dataKey="month",
+    data=data,
+    series=[
+        {"name": "Smartphones", "color": "violet.6", "stackId": "a"},
+        {"name": "Laptops", "color": "blue.6", "stackId": "b"},
+        {"name": "Tablets", "color": "teal.6", "stackId": "b"},
+    ]
+)
