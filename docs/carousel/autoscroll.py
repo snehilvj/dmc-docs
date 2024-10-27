3d4503f659b33e39a482d16fb3ac7567254bb819
Marko Marfat <marko.marfat@fer.hr>
2024-10-27 20:43:44 +0100

    Added autoscroll option section to Carousel docs

---
+++ docs/carousel/autoscroll.py
@@ -0,0 +1,12 @@
+import dash_mantine_components as dmc
+
+component = dmc.Carousel(
+    [
+        dmc.CarouselSlide(dmc.Center("Slide-1", bg="blue", c="white", p=60)),
+        dmc.CarouselSlide(dmc.Center("Slide-2", bg="blue", c="white", p=60)),
+        dmc.CarouselSlide(dmc.Center("Slide-3", bg="blue", c="white", p=60)),
+    ],
+    id="carousel-autoscroll",
+    loop=True,
+    autoScroll=True,
+)
