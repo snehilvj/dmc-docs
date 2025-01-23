32bd1305cc9909f57bca11e9fc1403a4c04aa5a5
AnnMarueW <amward@fastmail.com>
2025-01-23 07:44:03 -0800

    add container queries to carousel

---
+++ docs/carousel/containerquery.py
@@ -0,0 +1,27 @@
+import dash_mantine_components as dmc
+from dash import html
+
+component = html.Div(
+    # Wrapper div is added for demonstration purposes only,
+    #  It is not required in real projects
+    dmc.Carousel(
+        [
+            dmc.CarouselSlide(dmc.Center("Slide-1", bg="blue", c="white", p=60)),
+            dmc.CarouselSlide(dmc.Center("Slide-2", bg="blue", c="white", p=60)),
+            dmc.CarouselSlide(dmc.Center("Slide-3", bg="blue", c="white", p=60)),
+        ],
+        id="carousel-container",
+        type="container",
+        slideSize = {"base": '100%', '300px': '50%', '500px': '33.333333%'},
+        slideGap = {"base": 0, '300px': 'md', '500px': 'lg'},
+        loop=True,
+        align = "start"
+    ),
+    style={
+        "resize": 'horizontal',
+        "overflow": 'hidden',
+        "maxWidth": '100%',
+        "minWidth": 250,
+        "padding": 10,
+      }
+)
