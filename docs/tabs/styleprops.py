e9efaf9dd08c3bfabc86ee3369d447c445498544
AnnMarueW <amward@fastmail.com>
2025-01-24 20:38:43 -0800

    added more styling exmples

--- docs/tabs/styleprops.py
+++ docs/tabs/styleprops.py
@@ -5,13 +5,18 @@ component = dmc.Tabs(
         dmc.TabsList(
             [
                 dmc.TabsTab("Gallery", value="gallery"),
-                dmc.TabsTab("Messages", value="messages", disabled=True),
+                dmc.TabsTab("Messages", value="messages"),
                 dmc.TabsTab("Settings", value="settings"),
-            ]
+            ],
+            grow=True,
+            bd="1px solid var(--mantine-color-default-border)"
         ),
         dmc.TabsPanel("Gallery tab content", value="gallery"),
         dmc.TabsPanel("Messages tab content", value="messages"),
         dmc.TabsPanel("Settings tab content", value="settings"),
     ],
-    value="gallery"
+    color="green.3",
+    autoContrast=True,
+    variant="pills",
+    value="gallery",
 )
