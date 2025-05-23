3ccb1f08814dfdb8b476ce20e1d5dce7250b6854
StephaneBranly <stephanebranly.pro@gmail.com>
2025-05-23 18:55:29 +0200

    [~] Fix tabs example

--- docs/tabs/position.py
+++ docs/tabs/position.py
@@ -3,25 +3,25 @@ import dash_mantine_components as dmc
 from lib.configurator import Configurator
 
 # to fix, target ID on tabsList
-target = dmc.Tabs(
-    [
-        dmc.TabsList(
+target = dmc.TabsList(
             [
                 dmc.TabsTab("Gallery", value="gallery"),
                 dmc.TabsTab("Messages", value="messages"),
                 dmc.TabsTab("Settings", value="settings"),
             ],
-        ),
-    ],
-    value="gallery",
-)
+        )
 
-
-configurator = Configurator(target, center_component=True)
+configurator = Configurator(target)
 
 configurator.add_select(
     "justify", ["flex-start", "center", "flex-end", "space-around"], "center"
 )
 configurator.add_switch("grow", False)
 
-component = configurator.panel
+component = dmc.Tabs(
+    [
+        configurator.panel
+    ],
+    value="gallery",
+)
+
