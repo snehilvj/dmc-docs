945d81ab345d1858c4f68f531650f2a623e5ca60
AnnMarueW <amward@fastmail.com>
2025-05-24 15:13:24 -0700

    add function examples to Slider

--- docs/slider/label.py
+++ docs/slider/label.py
@@ -17,3 +17,33 @@ component = dmc.Stack(
         ),
     ]
 )
+
+
+
+import dash_mantine_components as dmc
+
+component = dmc.MantineProvider([
+    dmc.Text("No label", size="sm"),
+    dmc.Slider(value=40, label=None),
+
+    dmc.Text("Formatted label", size="sm", mt="xl"),
+    dmc.Slider(
+        value=40,
+        label={"function": "celsiusLabel"}
+    ),
+
+    dmc.Text("Label always visible", size="sm", mt="xl"),
+    dmc.Slider(value=40, labelAlwaysOn=True),
+
+    dmc.Text("Custom label transition", size="sm", mt="xl"),
+    dmc.Slider(
+        value=40,
+        labelTransitionProps={
+            "transition": "skew-down",
+            "duration": 150,
+            "timingFunction": "linear"
+        }
+    ),
+])
+
+
