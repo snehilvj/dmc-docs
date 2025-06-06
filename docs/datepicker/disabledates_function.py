a8e75a2ade014a9ca119345a7aad2ba824af0fd4
AnnMarueW <amward@fastmail.com>
2025-06-06 06:24:31 -0700

    updated disabled dates example

--- docs/datepicker/disabledates_function.py
+++ docs/datepicker/disabledates_function.py
@@ -1,6 +1,9 @@
 
 import dash_mantine_components as dmc
 
+# makes dayjs available
+#app = Dash(exteral_scripts=["https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.10.8/dayjs.min.js"])
+
 component = dmc.Center(
     dmc.DatePicker(
         value="2024-11-08",
