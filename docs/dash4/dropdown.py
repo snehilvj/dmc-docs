65ef96ca87f6fa194ef58bdcc3e08e1d8d5532bb
AnnMarueW <amward@fastmail.com>
2026-02-08 12:57:19 -0800

    update for dash4

---
+++ docs/dash4/dropdown.py
@@ -0,0 +1,29 @@
+import dash_mantine_components as dmc
+from dash import dcc
+
+component = dmc.Card([
+    dmc.SimpleGrid(
+        [
+            dmc.InputWrapper(
+                dcc.Dropdown([f"option {i}" for i in range(100)],  value=["option 1", "option 2", "option 3"], multi=True, id="dcc-dropdown"),
+                label="dcc.Dropdown with a dmc.InputWrapper",
+                htmlFor="dcc-dropdown"
+            ),
+            dmc.NumberInput(
+                label="dmc.NumberInput",
+                value=30712,
+                thousandSeparator=True,
+                prefix="$ ",
+
+            ),
+            dmc.DatePickerInput(label="dmc.DatePickerInput", value="2025-12-05"),
+        ],
+        cols={"base": 1, "md": 3},
+    ),
+    ],
+    withBorder=True,
+    shadow="sm",
+    radius="md",
+    p="md",
+    className="dmc"  # applies Mantine styles
+)
